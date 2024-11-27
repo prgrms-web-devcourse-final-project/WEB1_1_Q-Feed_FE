dbb64688e2d9c87fce565636de28e02f9ad629bf
MinSeok Kim <kimminsuk051@gmail.com>
2024-11-27 10:05:03 +0900

    refactor: qfeed-51 svg import

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -5,6 +5,8 @@ declare module '*.svg?react' {
 }
 
 declare module '*.svg' {
-  const content: string;
-  export default content;
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
 }
