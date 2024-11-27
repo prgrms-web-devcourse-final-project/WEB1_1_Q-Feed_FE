23dc27df0d29434c6360135526baef1257170828
jymaeng <jymaeng1234@gmail.com>
2024-11-27 17:57:14 +0900

    feat: qpeed-49 Footer 기능 구현

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import RootLayout from '@/components/RootLayout';
+import Login from '@/pages/Login';
 import Main from '@/pages/Main';
 import { createBrowserRouter } from 'react-router-dom';
 
@@ -9,6 +10,18 @@ const router = createBrowserRouter([
     children: [
       {
         path: '', // 홈 페이지
+        element: <Main />,
+      },
+      {
+        path: '/qspace', // 홈 페이지
+        element: <Login />,
+      },
+      {
+        path: 'chat', // 홈 페이지
+        element: <Main />,
+      },
+      {
+        path: '/mypage', // 홈 페이지
         element: <Main />,
       },
     ],
