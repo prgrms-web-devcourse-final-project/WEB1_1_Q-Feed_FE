a566e2125c363770a769ce1874121016e1d75e68
MinSeok Kim <kimminsuk051@gmail.com>
2024-11-28 17:11:16 +0900

    feat: qfeed-66 KebabMenu 컴포넌트 추가

--- src/pages/QSpaceDetail/components/MemberListButton/MemberListButton.stories.tsx
+++ src/pages/QSpaceDetail/components/MemberListButton/MemberListButton.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import MemberListButton from './MemberListButton';
 
 const meta = {
-  title: 'Components/MemberListButton',
+  title: 'Components/QSpaceDetail/MemberListButton',
   component: MemberListButton,
   parameters: {
     layout: 'centered',
