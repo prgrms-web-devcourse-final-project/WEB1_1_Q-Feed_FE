f909e994d0995eef9aaa2a5f8744aadc91ce42af
jymaeng <jymaeng1234@gmail.com>
2024-11-28 11:34:15 +0900

    feat: qfeed-63 답변카드 컴포넌트 추가

---
+++ src/pages/Main/components/AnswerCard/AnswerCard.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+import theme from '@/styles/theme';
+
+type AnswerCardProps = {
+  date : string;
+  question : string;
+};
+
+export const AnswerCard = ({ date, question }: AnswerCardProps) => {
+  return (
+    <Container>
+      <TextDate>{date}</TextDate>
+      <TextAnswer>{question}</TextAnswer>
+    </Container>
+  );
+};
+
+const TextDate = styled.h1`
+  width : 100%;
+  font-family : 'Noto Sans KR';
+  font-size : 16px;
+  font-weight: normal;
+  color : ${theme.colors.gray[300]};
+  text-align: center;
+
+`;const TextAnswer = styled.h2`
+width: 100%;
+  font-family : 'Noto Sans KR';
+  font-size : 16px;
+  font-weight: normal;
+  color: ${theme.colors.gray[600]};
+  text-align: center;
+  word-wrap: break-word;
+  overflow-wrap: break-word;
+  white-space: pre-wrap;
+  margin-top:15px;
+`;
+
+const Container = styled.div`
+width: 100%;
+min-height: 88px;
+height: auto;
+max-width: 377px;
+background: white;
+display: flex;
+flex-direction: column;
+align-items: center;
+border-radius: 15px;
+box-shadow: 0 10px 16px 0 rgba(0, 0, 0, 0.2);
+padding: 20px 40px;
+`;
