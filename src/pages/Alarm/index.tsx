c2f42b47659a3699e95d294ddb8cf7569fe785d6
ahnjongin <goodtime1998@naver.com>
2024-12-28 16:58:18 +0900

    fix: qfeed-148 알림리스트 타입 별 구분, 팔로우 버튼

--- src/pages/Alarm/index.tsx
+++ src/pages/Alarm/index.tsx
@@ -2,29 +2,18 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { IoChevronBack } from 'react-icons/io5';
-import ProfileImage from '@/components/ui/ProfileImageCon/ProfileImageCon';
+import NotificationItemComponent from '@/pages/Alarm/AlarmItem';
 import {
-  backIconStyle,
   containerStyle,
   headerStyle,
   headerTitleStyle,
-  listCon,
-  listConRead,
-  listStyle,
-  markAllAsReadStyle,
-  notificationContentStyle,
-  notificationMessageStyle,
-  notificationTypeStyle,
   readWrap,
-  timeStyle,
   unreadCountStyle,
+  markAllAsReadStyle,
+  listStyle,
 } from '@/pages/Alarm/styles';
 import { NotificationItem } from '@/pages/Alarm/type/alarmType';
-import {
-  fetchNotifications,
-  markNotificationAsRead,
-  markAllNotificationsAsRead,
-} from '@/pages/Alarm/api/fetchAlarm';
+import { fetchNotifications, markAllNotificationsAsRead } from '@/pages/Alarm/api/fetchAlarm';
 
 const NotificationPage = () => {
   const navigate = useNavigate();
@@ -39,7 +28,9 @@ const NotificationPage = () => {
       try {
         const data = await fetchNotifications();
         setNotifications(data);
-        const readIds = data.filter((notification) => notification.isRead).map((n) => n.id);
+        const readIds = data
+          .filter((notification) => notification.isRead)
+          .map((n) => n.notificationId);
         setReadItems(readIds); // 이미 읽음 처리된 알림 ID를 상태에 저장
       } catch (error) {
         console.error('알림 데이터를 불러오는 중 오류가 발생했습니다:', error);
@@ -49,25 +40,16 @@ const NotificationPage = () => {
     loadNotifications();
   }, []);
 
-  // 알림 클릭 시 읽음 처리 및 URL로 이동
-  const handleItemClick = async (notification: NotificationItem) => {
-    if (!readItems.includes(notification.id)) {
-      // 읽음 처리 API 호출
-      await markNotificationAsRead(notification.id);
-      setReadItems((prev) => [...prev, notification.id]);
-    }
-
-    // URL로 이동
-    if (notification.url) {
-      navigate(notification.url);
-    }
+  // 특정 알림 읽음 처리
+  const handleItemRead = (id: number) => {
+    setReadItems((prev) => [...prev, id]);
   };
 
   // 모든 알림 읽음 처리
-  const markAllAsRead = async () => {
+  const handleMarkAllAsRead = async () => {
     try {
       await markAllNotificationsAsRead(); // API 호출
-      setReadItems(notifications.map((notification) => notification.id)); // 모든 알림을 읽음 처리 상태로 변경
+      setReadItems(notifications.map((notification) => notification.notificationId)); // 모든 알림을 읽음 처리 상태로 변경
     } catch (error) {
       console.error('모든 알림 읽음 처리 중 오류가 발생했습니다:', error);
     }
@@ -77,16 +59,14 @@ const NotificationPage = () => {
     <div css={containerStyle}>
       {/* Header */}
       <div css={headerStyle}>
-        <IoChevronBack css={backIconStyle} onClick={() => navigate(-1)} />
+        <IoChevronBack onClick={() => navigate(-1)} />
         <span css={headerTitleStyle}>알림</span>
       </div>
 
       {/* Unread count */}
       <div css={readWrap}>
-        <div css={unreadCountStyle}>
-          안읽은 알림 {notifications.length - readItems.length}개
-        </div>
-        <span css={markAllAsReadStyle} onClick={markAllAsRead}>
+        <div css={unreadCountStyle}>안읽은 알림 {notifications.length - readItems.length}개</div>
+        <span css={markAllAsReadStyle} onClick={handleMarkAllAsRead}>
           모두 읽음 표시
         </span>
       </div>
@@ -94,21 +74,12 @@ const NotificationPage = () => {
       {/* Notification List */}
       <div css={listStyle}>
         {notifications.map((notification) => (
-          <div
-            key={notification.id}
-            css={[
-              listCon,
-              readItems.includes(notification.id) && listConRead, // 읽음 처리된 항목 스타일
-            ]}
-            onClick={() => handleItemClick(notification)}
-          >
-            <ProfileImage src="" size={40} />
-            <div css={notificationContentStyle}>
-              <span css={notificationTypeStyle}>{notification.type}</span>
-              <p css={notificationMessageStyle}>{notification.message}</p>
-            </div>
-            <span css={timeStyle}>{notification.time}</span>
-          </div>
+          <NotificationItemComponent
+            key={notification.notificationId}
+            notification={notification}
+            isRead={readItems.includes(notification.notificationId)}
+            onRead={handleItemRead}
+          />
         ))}
       </div>
     </div>
--- src/pages/Alarm/type/alarmType.ts
+++ src/pages/Alarm/type/alarmType.ts
@@ -1,7 +1,9 @@
 export interface NotificationItem {
-  id: number; // 알림 ID
+  notificationId: number; // 알림 ID
   type: string; // 알림 유형 (예: 댓글, 좋아요 등)
-  message: string; // 알림 메시지 내용
+  content: string; // 알림 메시지 내용
+  sender: string; //알림 보내는 사람
+  recipient: string; //알림 받는 사람
   time: string; // 알림 생성 시간
   isRead: boolean; // 알림 읽음 여부
   url?: string;
