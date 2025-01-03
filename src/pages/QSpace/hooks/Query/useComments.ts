4d3365ca306df33aff761865d064476ed06aced9
MinSeok Kim <kimminsuk051@gmail.com>
2025-01-03 17:20:02 +0900

    feat: qfeed-159 replycontainer에 댓글 갯수 반영

--- src/pages/QSpace/hooks/Query/useComments.ts
+++ src/pages/QSpace/hooks/Query/useComments.ts
@@ -1,18 +1,37 @@
+// useComments.ts
 import { GROUP_KEYS } from '@/api/queryKeys';
 import { groupAPI } from '@/pages/QSpace/api/groupAPI';
 import { Reply } from '@/pages/QSpace/types/group';
-import { useQuery } from '@tanstack/react-query';
+import { useQueries, useQuery } from '@tanstack/react-query';
 
-export const useComments = (groupPostId: number) => {
+// 단일 댓글의 답글을 가져오는 Hook
+export const useSingleComments = (postId: number) => {
   return useQuery<Reply[], Error>({
-    queryKey: [GROUP_KEYS.ROOT, GROUP_KEYS.ACTIONS.COMMENTS, groupPostId],
+    queryKey: [GROUP_KEYS.ROOT, GROUP_KEYS.ACTIONS.COMMENTS, postId],
     queryFn: async () => {
-      const response = await groupAPI.getComments(groupPostId);
+      const response = await groupAPI.getComments(postId);
       if (!response.success || !response.data) {
         throw new Error(response.error?.message || 'Failed to fetch comments');
       }
       return response.data;
     },
-    enabled: !!groupPostId,
+    enabled: postId > 0,
+  });
+};
+
+// 여러 댓글의 답글을 가져오는 함수
+export const useCommentsQueries = (postIds: number[]) => {
+  return useQueries({
+    queries: postIds.map((id) => ({
+      queryKey: [GROUP_KEYS.ROOT, GROUP_KEYS.ACTIONS.COMMENTS, id],
+      queryFn: async () => {
+        const response = await groupAPI.getComments(id);
+        if (!response.success || !response.data) {
+          throw new Error(response.error?.message || 'Failed to fetch comments');
+        }
+        return response.data;
+      },
+      enabled: id > 0,
+    })),
   });
 };
